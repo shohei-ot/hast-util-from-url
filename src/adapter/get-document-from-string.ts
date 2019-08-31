8f74be85088faef2f2091576e021e468c0f485db
Shohei Ohta <shohei.ot@gmail.com>
2019-08-31 20:18:42 +0900

    fix: browser support

--- src/adapter/get-document-from-string.ts
+++ src/adapter/get-document-from-string.ts
@@ -1,10 +1,18 @@
 import isBrowser from 'is-in-browser'
 import { JSDOM } from 'jsdom'
 
-export default (html: string): Document => {
-  if (isBrowser) {
-    return new DOMParser().parseFromString(html, 'text/html')
-  }
+export const fromStringBrowser = (html: string) => {
+  return new DOMParser().parseFromString(html, 'text/html')
+}
+
+export const fromStringNode = (html: string) => {
   const { window } = new JSDOM(html)
   return window.document
+}
+
+export default (html: string): Document => {
+  if (isBrowser) {
+    return fromStringBrowser(html)
+  }
+  return fromStringNode(html)
 }
--- src/proxy/get-response-body.ts
+++ src/proxy/get-response-body.ts
@@ -1,46 +1,15 @@
-import isBrowser from 'is-in-browser'
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 
-const http = require('http')
-const https = require('https')
-const isUrl = require('is-url')
-
-const isHttps = (url: string) => isUrl(url) && /^https/.test(url)
-
-export const getResponseBodyNodeJs = async (url: string): Promise<string> => {
-  const client = isHttps(url) ? https : http
-  const resp: string = await new Promise((resolve, reject) => {
-    let buff: string = ''
-
-    const req = client.request(url, (res: any) => {
-      res.setEncoding('utf8')
-      res.on('data', (chunk: string) => {
-        buff += chunk
-      })
-
-      res.on('end', () => {
-        if (res.statusCode !== 200) {
-          reject(new Error('not 200'))
-        } else {
-          resolve(buff)
-        }
-      })
-    })
-    req.end()
-  })
-  return resp
-}
-
-export const getResponseBodyBrowser = async (url: string): Promise<string> => {
-  const response = await axios.get(url, {
-    responseType: 'text'
-  })
+export default async (
+  url: string,
+  requestConfig: Partial<AxiosRequestConfig> = {}
+): Promise<string> => {
+  const axiosConfig: Partial<AxiosRequestConfig> = Object.assign(
+    requestConfig,
+    {
+      responseType: 'text'
+    }
+  )
+  const response = await axios.get(url, axiosConfig)
   return response.data
-}
-
-export default (url: string) => {
-  if (isBrowser) {
-    return getResponseBodyBrowser(url)
-  }
-  return getResponseBodyNodeJs(url)
 }
