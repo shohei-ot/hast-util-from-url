c649c25626036cc544a6664399bdf12144a24985
Shohei Ohta <shohei.ot@gmail.com>
2019-08-31 18:19:26 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,9 @@
-const exit = require('exit')
 const { JSDOM } = require('jsdom')
 const fromDOM = require('hast-util-from-dom')
 const getResponseBody = require('./proxy/get-response-body')
 
 module.exports = async function(url: string) {
-  let body = ''
-  try {
-    body = await getResponseBody(url)
-  } catch (e) {
-    console.log('Error')
-    console.log(e.message)
-    exit(1)
-  }
-  // console.log(body)
+  const body = await getResponseBody(url)
   const { window } = new JSDOM(body)
-  const hast = fromDOM(window.document)
-
-  return hast
+  return fromDOM(window.document)
 }
